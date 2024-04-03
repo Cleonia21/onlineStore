4cf8128f091b596c5002a3becf9c80909a27a9e0
cleonia <cleonia@yandex.ru>
2024-04-03 19:50:59 +0300

    add sql code

---
+++ createTables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Products
+(
+    Id SERIAL PRIMARY KEY,
+    Name text NOT NULL,
+    MainShelvingId INTEGER,
+    FOREIGN KEY (MainShelvingId) REFERENCES Shelving (Id) ON DELETE RESTRICT
+);
+
+CREATE TABLE Shelving
+(
+    Id SERIAL PRIMARY KEY,
+    Name CHARACTER(1) NOT NULL
+);
+
+CREATE TABLE Orders
+(
+    Id SERIAL PRIMARY KEY,
+    Number INTEGER NOT NULL,
+    ProductId INTEGER,
+    Quantity INTEGER NOT NULL,
+    FOREIGN KEY (ProductId) REFERENCES Products (Id)
+);
+
+CREATE TABLE ProductsOnTheShelving
+(
+    Id SERIAL PRIMARY KEY,
+    ShelvingId INTEGER,
+    ProductId INTEGER,
+    FOREIGN KEY (ShelvingId) REFERENCES Shelving (Id) ON DELETE RESTRICT,
+    FOREIGN KEY (ProductId) REFERENCES Products (Id) ON DELETE RESTRICT
+);
