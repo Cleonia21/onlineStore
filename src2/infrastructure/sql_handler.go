e4f487b8fc829a511fde334677795e32d2e6360e
cleonia <cleonia@yandex.ru>
2024-04-10 15:40:25 +0300

    Добавил реализацию запросов к БД

--- src2/infrastructure/sql_handler.go
+++ src2/infrastructure/sql_handler.go
@@ -3,6 +3,9 @@ package infrastructure
 import (
 	"database/sql"
 	"fmt"
+	"onlineStore/src2/interfaces/database"
+	"strconv"
+	"strings"
 )
 
 type SqlHandler struct {
@@ -25,6 +28,98 @@ func NewSqlHandler() *SqlHandler {
 	return sqlHandler
 }
 
-func (handler *SqlHandler) FindSome(id int) string {
-	return ""
+func (handler *SqlHandler) GetOrders(id int) ([]database.Order, error) {
+	q := `SELECT (productId, quantity) FROM orders WHERE number = %v`
+	q = fmt.Sprintf(q, id)
+
+	rows, err := handler.db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var orders []database.Order
+	for rows.Next() {
+		var order database.Order
+		err = rows.Scan(&order.ProductId, &order.Quantity)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, order)
+	}
+	return orders, err
+}
+
+func (handler *SqlHandler) GetProducts(id []int) ([]database.Product, error) {
+	q := `SELECT (id, name, shelfId) FROM products WHERE id IN (%v)`
+	q = fmt.Sprintf(q, intToStrJoin(id, ","))
+
+	rows, err := handler.db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []database.Product
+	for rows.Next() {
+		var product database.Product
+		err = rows.Scan(&product.Id, &product.Name, &product.ShelfId)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	return products, err
+}
+
+func (handler *SqlHandler) GetShelving(id []int) ([]database.Shelf, error) {
+	q := `SELECT (id, name) FROM shelving WHERE id IN (%v)`
+	q = fmt.Sprintf(q, intToStrJoin(id, ","))
+
+	rows, err := handler.db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var shelving []database.Shelf
+	for rows.Next() {
+		var shelf database.Shelf
+		err = rows.Scan(&shelf.Id, &shelf.Name)
+		if err != nil {
+			return nil, err
+		}
+		shelving = append(shelving, shelf)
+	}
+	return shelving, err
+}
+
+func (handler *SqlHandler) GetOptionalShelving(ProductId []int) ([]database.OptionalShelving, error) {
+	q := `SELECT (productId, shelfId) FROM optionalShelving WHERE productId IN (%v)`
+	q = fmt.Sprintf(q, intToStrJoin(ProductId, ","))
+
+	rows, err := handler.db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var optionalShelving []database.OptionalShelving
+	for rows.Next() {
+		var optionalShelf database.OptionalShelving
+		err = rows.Scan(&optionalShelf.ProductId, &optionalShelf.ShelfId)
+		if err != nil {
+			return nil, err
+		}
+		optionalShelving = append(optionalShelving, optionalShelf)
+	}
+	return optionalShelving, err
+}
+
+func intToStrJoin(ordersNum []int, sep string) string {
+	var strs []string
+	for _, i := range ordersNum {
+		strs = append(strs, strconv.Itoa(i))
+	}
+	return strings.Join(strs, sep)
 }
